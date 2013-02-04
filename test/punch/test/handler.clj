873a845aeecae453fc175a4d262de484bb37f680
Philip Potter <philipgpotter@gmail.com>
2013-02-04 21:09:31 +0000

    rewrite expectations in another way

--- test/punch/test/handler.clj
+++ test/punch/test/handler.clj
@@ -4,6 +4,15 @@
         punch.handler)
   (:require [cheshire.core :refer [parse-string]]))
 
+
+(given (app (request :get "/"))
+       (expect
+        #(get-in % [:headers "Content-Type"]) "application/json"
+        :status    200
+        (comp set keys parse-string :body) #{"data" "document_type" "metadata"}
+        (comp set keys #(get % "data") parse-string :body) #{"name" "tags" "classes" "edges" "version" "resources"}
+        (comp in parse-string :body) {"document_type" "Catalog", "metadata" {"api_version" 1}})
+       )
 (let [response (app (request :get "/"))
       parsed-json (parse-string (:body response))]
   (expect "application/json" (get-in response [:headers "Content-Type"]))
