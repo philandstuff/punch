819fdb665e3c9e6ec9685e39648fd50549bbadb6
Philip Potter <philipgpotter@gmail.com>
2013-02-05 19:23:15 +0000

    move catalog into source file

--- src/punch/handler.clj
+++ src/punch/handler.clj
@@ -1,13 +1,69 @@
 (ns punch.handler
-  (:use compojure.core
-        [clojure.java.io :only [reader resource]])
+  (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.mock.request :refer [request]]
-            [cheshire.core :refer [generate-string parse-string]]))
+            [cheshire.core :refer [generate-string]]))
 
-(def blank-catalog (binding [*read-eval* false] ;; turn off EvalReader
-                     (read (java.io.PushbackReader. (reader (resource "blank-catalog.edn"))))))
+(def blank-catalog {
+  :data {
+    :name "foo.vm",
+    :tags [
+      "settings"
+    ],
+    :classes [
+      "settings"
+    ],
+    :edges [
+      {
+        :target "Class[Settings]",
+        :source "Stage[main]"
+      },
+      {
+        :target "Class[main]",
+        :source "Stage[main]"
+      }
+    ],
+    :version 1359994915,
+    :resources [
+      {
+        :exported false,
+        :tags [
+          "stage"
+        ],
+        :title "main",
+        :type "Stage",
+        :parameters {
+          :name "main"
+        }
+      },
+      {
+        :exported false,
+        :tags [
+          "class",
+          "settings"
+        ],
+        :title "Settings",
+        :type "Class"
+      },
+      {
+        :exported false,
+        :tags [
+          "class"
+        ],
+        :title "main",
+        :type "Class",
+        :parameters {
+          :name "main"
+        }
+      }
+    ]
+  },
+  :document_type "Catalog",
+  :metadata {
+    :api_version 1
+  }
+})
 
 (defn catalog-for [hostname]
   blank-catalog)
